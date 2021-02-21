45fe5d4fc600aac424df9ad3e8c405d7836b5e8b
Waaaaaaaake <>
2021-02-22 00:40:16 +0200

    add iterator logic for button objects

--- js_lesson/calc_project/calc.js
+++ js_lesson/calc_project/calc.js
@@ -6,6 +6,14 @@ var btn_multiply_id = "btn_multiply";
 var number1Obj = document.getElementById('first_number');    
 var number2Obj = document.getElementById('second_number');
 
+// let buttonPlus = document.getElementById(btn_plus_id);
+// let buttonMinus = document.getElementById(btn_minus_id);
+// let buttonMultiply = document.getElementById(btn_multiply_id);
+// let buttonDivision = document.getElementById(btn_division_id);
+
+let arrayOfButtons = document.getElementsByClassName('operation_button');
+// let arrayOfButtons = [buttonPlus, buttonMinus, buttonMultiply, buttonDivision];
+
 function makeOperation(operation) {
     num1 = Number(number1Obj.value);
     num2 = Number(number2Obj.value);
@@ -49,12 +57,15 @@ function onOperationButtonClick(eventObject) {
     makeOperation(operation);
 }
 
-var plusObj = document.getElementById(btn_plus_id);
-var minusObj = document.getElementById(btn_minus_id);
-var multiplyObj = document.getElementById(btn_multiply_id);
-var divisionObj = document.getElementById(btn_division_id);
-
-plusObj.addEventListener(type="click", listener=onOperationButtonClick);
-minusObj.addEventListener(type="click", listener=onOperationButtonClick);
-multiplyObj.addEventListener(type="click", listener=onOperationButtonClick);
-divisionObj.addEventListener(type="click", listener=onOperationButtonClick);
+for(let item of arrayOfButtons) {
+    item.addEventListener(type="click", listener=onOperationButtonClick);     
+}
+// var plusObj = document.getElementById(btn_plus_id);
+// var minusObj = document.getElementById(btn_minus_id);
+// var multiplyObj = document.getElementById(btn_multiply_id);
+// var divisionObj = document.getElementById(btn_division_id);
+// 
+// plusObj.addEventListener(type="click", listener=onOperationButtonClick);
+// minusObj.addEventListener(type="click", listener=onOperationButtonClick);
+// multiplyObj.addEventListener(type="click", listener=onOperationButtonClick);
+// divisionObj.addEventListener(type="click", listener=onOperationButtonClick);
